5ed64a85facbd56e5fe8c63f483849d68c59ebcf
BeLievEr031 <sandyrajak031@gmail.com>
2026-09-09 12:22:35 +0530

    learnt for loop

---
+++ 5_for/for.go
@@ -0,0 +1,44 @@
+package main
+
+// import "fmt"
+
+func main() {
+	// fmt.Println("for loop")
+
+	// i := 1
+
+	// for i <= 10 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// for j := 1; j <= 10; j += 1 {
+	// 	fmt.Println(j)
+	// }
+
+	// Infinite loop
+
+	// for {
+	// 	println(1)
+	// }
+
+	// Break and continue
+
+	// for j := 0; j <= 12; j++ {
+	// 	if j == 9 {
+	// 		break
+	// 	}
+
+	// 	if j%2 == 0 {
+	// 		continue
+	// 	}
+
+	// 	println(j)
+	// }
+
+	// range
+
+	for i := range 1 {
+		println((i))
+	}
+}
