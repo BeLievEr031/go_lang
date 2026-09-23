a2e5715b1de57a8f422b13f29965e044f162c5bb
BeLievEr031 <sandyrajak031@gmail.com>
2026-09-23 12:40:26 +0530

    learnt struct

---
+++ 16_struct/struct.go
@@ -0,0 +1,63 @@
+// Structs are custome Data Structures available in Go.
+// In Go we don't have Classes
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Student struct {
+	name          string
+	age           int
+	percentage    float32
+	isPass        bool
+	admissionDate time.Time
+}
+
+func newStudent(name string, age int, percentage float32, isPass bool, admissionDate time.Time) *Student {
+	newStudent := Student{
+		name:          name,
+		age:           age,
+		percentage:    percentage,
+		isPass:        isPass,
+		admissionDate: admissionDate,
+	}
+
+	return &newStudent
+}
+
+type Order struct {
+	productName string
+	price       float32
+	status      string
+	delivered   bool
+}
+
+func (o *Order) changeStatus(status string) {
+	o.status = status
+}
+
+func changeDelivery(o *Order, delivered bool) {
+	o.delivered = delivered
+}
+
+func main() {
+	order1 := Order{
+		productName: "Asus Tuf A15",
+		price:       84000,
+		status:      "PENDING",
+		delivered:   false,
+	}
+
+	fmt.Println(order1)
+	order1.changeStatus("PROCESSING")
+	fmt.Println(order1)
+
+	changeDelivery(&order1, true)
+	fmt.Println(order1)
+
+	student1 := newStudent("sandy", 24, 80.80, true, time.Now())
+
+	fmt.Println(student1.name)
+}
