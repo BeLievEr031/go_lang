45b49ca84270896c0e348b392a8858acbb03d792
BeLievEr031 <sandyrajak031@gmail.com>
2026-09-23 16:33:54 +0530

    learnt go-routines with waitGrp

--- 20_go_routines/go-routines.go
+++ 20_go_routines/go-routines.go
@@ -2,17 +2,43 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func task(id int) {
+// func task(id int) {
+// 	fmt.Println(id)
+// }
+
+func wgTask[T interface{}](id T, w *sync.WaitGroup) {
+	defer w.Done()
 	fmt.Println(id)
 }
 
+// func deferFn() {
+// 	defer func() {
+// 		fmt.Println("I will run after this function completion")
+// 	}()
+
+// 	fmt.Println("Starting fn.")
+// 	fmt.Println("Processing fn.")
+// 	fmt.Println("Ending fn.")
+// }
+
 func main() {
+	// for i := 1; i <= 10; i++ {
+	// 	go task(i)
+	// }
+
+	// time.Sleep(time.Second * 2)
+
+	// deferFn()
+
+	var wg sync.WaitGroup
+
 	for i := 1; i <= 10; i++ {
-		go task(i)
+		wg.Add(1)
+		go wgTask(i, &wg)
 	}
 
-	time.Sleep(time.Second * 2)
+	wg.Wait()
 }
